f4bd164b01918d850db4d5f5f5cf78ec868862d8
Charles Foster <cfoster0@stanford.edu>
2021-06-15 18:37:23 -0700

    Initial commit

---
+++ model.py
@@ -0,0 +1,133 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from torch import einsum
+from einops import rearrange
+
+from dataclasses import dataclass
+
+@dataclass
+class Config:
+    """Class for keeping track config variables."""
+    depth: int
+    heads: int
+    head_dim: int
+    vocab_size: int
+    expansion_factor: int = 4
+
+
+class Residual(nn.Module):
+    def __init__(self, residual):
+        """
+        In the constructor we stash way the module that'll be called along
+        the residual branch. This is just for convenience.
+        """
+        super(Residual, self).__init__()
+        self.residual = residual
+
+    def forward(self, x):
+        return x + self.residual(x)
+
+class RotaryEmbedding(nn.Module):
+    def __init__(self, config, max_seq_len=2048):
+        """
+        In the constructor we cache relative positional embeddings. When called
+        in forward(), will assume that whatever is passed represents the last
+        N positions, so this will not work for autoregressive w/ caching. For
+        an intuitive look into rotary positional embeddings, see the below link:
+        https://blog.eleuther.ai/rotary-embeddings/
+        """
+        super().__init__()
+        dim = config.head_dim
+        inv_freq = 1. / (10000 ** (torch.arange(0, dim, 2).float() / dim))
+        t = torch.arange(max_seq_len).type_as(inv_freq)
+        freqs = torch.einsum('i , j -> i j', t, inv_freq)
+        emb = torch.cat((freqs, freqs), dim=-1)
+        torch.register_buffer('freqs', rearrange(emb, 'n d -> () n () d'))
+
+    def rotate_half(self, x):
+        x = rearrange(x, '... (j d) -> ... j d', j = 2)
+        x1, x2 = x.unbind(dim = -2)
+        return torch.cat((-x2, x1), dim = -1)
+
+    def forward(self, x):
+        seq_len = x.shape[-3]
+        freqs = self.freqs[:, -seq_len:]
+        return (x * freqs.cos()) + (self.rotate_half(x) * freqs.sin())
+
+class Block(nn.Module):
+    def __init__(self, config: Config):
+        """
+        In this module is the code for a transformer block. This builds on
+        Ben Wang's ideas of (1) doing the attention and feedforward residual
+        computations in parallel instead of in series, which incidentally lets
+        them share a single layer norm, and (2) merging the Wq, Wk, & Wv
+        projections with the first FF projection and the Wo projection with the
+        second FF projection, which should simplify and speed up the design.
+        For a reference on these methods, see the below link:
+        https://github.com/kingoflolz/mesh-transformer-jax
+        """
+        super(Block, self).__init__()
+        self.heads = config.heads
+        self.head_dim = config.head_dim
+        self.hidden_dim = self.heads * self.head_dim
+        self.expansion_factor = config.expansion_factor
+        self.qkvl_dim = self.hidden_dim * (3 + self.expansion_factor)
+        self.vl_dim = self.hidden_dim * (1 + self.expansion_factor)
+
+        self.ln = nn.LayerNorm(self.hidden_dim)
+        self.in_proj = nn.Linear(self.hidden_dim, self.qkvl_dim, False)
+        self.out_proj = nn.Linear(self.vl_dim, self.hidden_dim)
+        self.rotary = RotaryEmbedding(config)
+
+    def forward(self, x):
+      x = self.ln(x)
+      x = self.in_proj(x)
+      q, k, v, p = torch.split(x, [
+                                   self.hidden_dim,
+                                   self.hidden_dim,
+                                   self.hidden_dim,
+                                   self.hidden_dim * self.expansion_factor
+                                   ], -1)
+      (q, k, v) = map(lambda x: rearrange(x, "b i (h d) -> b i h d", h=self.heads), (q, k, v))
+      (q, k) = map(lambda x: self.rotary(x), (q, k))
+      a = einsum("bihd,bjhd -> bijh", q, k)
+      o = einsum("bijh,bjhd -> bihd", a, v)
+      o = rearrange(o, "b i h d -> b i (h d)")
+      p = F.gelu(p)
+      x = torch.cat([o, p], dim=-1)
+      x = self.out_proj(x)
+      return x
+
+class Transformer(nn.Module):
+    def __init__(self, config: Config):
+        """
+        In this module is the code for the model as a whole, including the
+        prelude (where tokens become hidden representations), the body (where
+        hidden representations are transformed), and the postlude (where
+        hidden representations become token distributions).
+        """
+        super(Transformer, self).__init__()
+        hidden_dim = config.heads * config.head_dim
+        prelude = nn.Sequential(*[
+                                  nn.Embedding(config.vocab_size, hidden_dim),
+                                  ])
+        body = nn.Sequential(*[Residual(Block(config)) for _ in range(config.depth)])
+
+        postlude = nn.Sequential(*[
+                                  nn.LayerNorm((hidden_dim)),
+                                  nn.Linear(hidden_dim, config.vocab_size, True),
+                                  nn.Softmax(dim=-1),
+                                  ])
+        
+        network = nn.Sequential(*[
+                                  prelude, 
+                                  body, 
+                                  postlude,
+                                  ])
+
+        self.network = network
+
+    def forward(self, x):
+        return self.network(x)
