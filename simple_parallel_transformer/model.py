f5b4c3174c1ce498724897acddd253fa4530691d
Charles Foster <cfoster0@stanford.edu>
2023-04-14 15:31:39 -0700

    Update to new 04-2023 baseline

--- simple_parallel_transformer/model.py
+++ simple_parallel_transformer/model.py
@@ -2,15 +2,11 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from torch import einsum
 from einops import rearrange
-
 from opt_einsum import contract
-
 from dataclasses import dataclass
 from hydra.core.config_store import ConfigStore
 
-from .extra_modules import *
 
 @dataclass
 class Config:
@@ -20,6 +16,7 @@ class Config:
     head_dim: int
     vocab_size: int
     max_seq_len: int = 2048
+    expansion_factor: int = 4
     seed: int = 10101
         
 cs = ConfigStore.instance()
@@ -29,165 +26,128 @@ cs.store(name="config", node=Config)
 def exists(val):
     return val is not None
 
-class AlibiPositionalBias(nn.Module):
+def shift(x, dimensions, n):
+  return torch.cat([F.pad(x[..., :dimensions], (0, 0, n, -n), value=0), x[..., dimensions:]], dim=-1)
+
+class LearnedALiBi(nn.Module):
     def __init__(self, heads):
         super().__init__()
-        self.heads = heads
-        slopes = torch.Tensor(self._get_slopes(heads))
-        slopes = rearrange(slopes, 'h -> () h () ()')
-        self.register_buffer('slopes', slopes, persistent = False)
+        self.slopes = nn.Parameter(rearrange(torch.logspace(0., -6., steps=heads, base=2.), 'h -> () h () ()'))
         self.register_buffer('bias', None, persistent = False)
-
-    @staticmethod
-    def _get_slopes(heads):
-        import math
-        def get_slopes_power_of_2(n):
-            start = (2**(-2**-(math.log2(n)-3)))
-            ratio = start
-            return [start*ratio**i for i in range(n)]
-
-        if math.log2(heads).is_integer():
-            return get_slopes_power_of_2(heads)
-
-        closest_power_of_2 = 2 ** math.floor(math.log2(heads))
-        return get_slopes_power_of_2(closest_power_of_2) + get_slopes_power_of_2(2 * closest_power_of_2)[0::2][:heads-closest_power_of_2]
 
     def forward(self, qk_dots):
         h, i, j, device = *qk_dots.shape[-3:], qk_dots.device
 
-        if exists(self.bias) and self.bias.shape[-1] >= j:
+        if (not self.training) and exists(self.bias) and self.bias.shape[-1] >= j:
             return qk_dots + self.bias[..., :j]
 
-        bias = torch.arange(j, device = device)
-        bias = rearrange(bias, 'j -> () () () j')
-        bias = bias * self.slopes
+        bias = rearrange(torch.arange(j, device = device), 'j -> () () () j') * self.slopes
         bias = F.pad(bias, (0, 0, 0, 0, 0, h - bias.shape[1]))
         self.register_buffer('bias', bias, persistent = False)
-        return qk_dots + self.bias
-      
-class Shift(nn.Module):
-    def __init__(self, dimensions, n):
-        super(Shift, self).__init__()
-        self.dimensions = dimensions
-        self.n = n
-
-    def forward(self, x):
-        part = x[..., :self.dimensions]
-        x[..., :self.dimensions] = F.pad(part, (0, 0, self.n, -self.n), value=0)
-        return x
+        return qk_dots + bias
     
-class Residual(nn.Module):
-    def __init__(self, residual):
-        """
-        In the constructor we stash way the module that'll be called along
-        the residual branch. This is just for convenience.
-        """
-        super(Residual, self).__init__()
-        self.residual = residual
-
-    def forward(self, x, attn):
-        x_residual, attn_residual = self.residual(x, attn)
-        return x + x_residual, attn + attn_residual
-
 class Block(nn.Module):
-    def __init__(self, config: Config, layer_depth: int):
+    def __init__(self, config: Config):
         """
         In this module is the code for a transformer block. This builds on
-        Ben Wang's ideas of (1) doing the attention and feedforward residual
-        computations in parallel instead of in series, which incidentally lets
-        them share a single layer norm, and (2) merging the Wq, Wk, & Wv
-        projections with the first FF projection and the Wo projection with the
-        second FF projection, which should simplify and speed up the design.
-        For a reference on these methods, see the below link:
-        https://github.com/kingoflolz/mesh-transformer-jax
+        several ideas: (1) Ben Wang's parallel attention and feedforward block
+        (2) Cogview's Sandwich Layer Norm, which puts a Layer Norm at the start 
+        and end of the block (3) ALiBi-like per-head learned linear biases on 
+        the attention similar to ALiBi (4) BlinkDL's token shift on a portion
+        of dimensions to provide direct access to previous token representations
+        & easy n-gram learning and (5) a home-grown modification to GeGLU that
+        uses a single projection matrix and uses a rolled version of the original
+        representation to do the linear modulation (6) a home-grown modification
+        that adds a ReLU + LayerNorm after the initial projection & token shift.
+        ---
+        References:
+        Parallel Block - https://github.com/kingoflolz/mesh-transformer-jax
+        Sandwich Layer Norm - https://arxiv.org/abs/2105.13290
+        ALiBi - https://arxiv.org/abs/2108.12409
+        Token Shift - https://github.com/BlinkDL/RWKV-LM
+        GeGLU - https://arxiv.org/abs/2002.05202v1
         """
         super(Block, self).__init__()
         self.heads = config.heads
         self.head_dim = config.head_dim
-        self.hidden_dim = self.heads * self.head_dim
         self.max_seq_len = config.max_seq_len
-        self.qkv_dim = self.hidden_dim * (3 + 2)
-        self.vp_dim = self.hidden_dim * (1 + 1)
+        self.expansion_factor = config.expansion_factor
+        self.hidden_dim = self.heads * self.head_dim
+
+        qkvp_dim = self.hidden_dim * (3 + self.expansion_factor)
+        vp_dim = self.hidden_dim * (1 + self.expansion_factor)
 
         self.in_ln = nn.LayerNorm(self.hidden_dim)
-        self.mid_ln = nn.LayerNorm(self.hidden_dim * 2)
-        self.q_ln = nn.LayerNorm(self.hidden_dim)
-        self.k_ln = nn.LayerNorm(self.hidden_dim)
-
+        self.mid_ln = nn.LayerNorm(qkvp_dim)
         self.out_ln = nn.LayerNorm(self.hidden_dim)
-        self.shift = Shift(self.hidden_dim // 4, 1)
-        self.in_proj = nn.Linear(self.hidden_dim, self.qkv_dim, False)
-        nn.init.orthogonal_(self.in_proj.weight)
-        self.out_proj = nn.Linear(self.vp_dim, self.hidden_dim, True)
-        nn.init.zeros_(self.out_proj.weight)
-        self.alibi = AlibiPositionalBias(self.heads)
 
-        self.mixing_tensor = nn.Parameter(torch.randn(self.hidden_dim, self.hidden_dim))
+        self.in_proj = nn.Linear(self.hidden_dim, qkvp_dim, False)
+        self.out_proj = nn.Linear(vp_dim, self.hidden_dim, True)
+        nn.init.orthogonal_(self.in_proj.weight)
+        nn.init.zeros_(self.out_proj.weight)
+        nn.init.zeros_(self.out_proj.bias)
 
         causal_mask = torch.tril(torch.ones((self.max_seq_len, self.max_seq_len)))
         causal_bias = -1e10 * (1. - causal_mask)
         self.register_buffer('causal_bias', rearrange(causal_bias, "i j -> () () i j"))
+        self.alibi = LearnedALiBi(self.heads)
 
-    def forward(self, x, accumulated_logits):
+    def forward(self, x):
         b, l, d = x.shape
 
         x = self.in_ln(x)
-        x[..., :d//4] = self.shift(x[..., :d//4])
-        y = self.in_proj(x)
-        q, k, v, p = torch.split(y, [
+        x[..., :d//4] = shift(x[..., :d//4], self.hidden_dim // 4, 1)
+        x[..., d//8:] = shift(x[..., d//8:], self.hidden_dim // 8, 2)
+        x = self.mid_ln(F.relu(self.in_proj(x)))
+        q, k, v, p = torch.split(x, [
                                    self.hidden_dim,
                                    self.hidden_dim,
                                    self.hidden_dim,
-                                   self.hidden_dim * 2,
+                                   self.hidden_dim * self.expansion_factor
                                    ], -1)
-        q = self.q_ln(q)
-        k = self.k_ln(k)
-        (q, k, v) = map(lambda x: rearrange(x, "b i (h d) -> b h i d", h=self.heads, b=b), (q, k, v))
-        logits = einsum("b h i d, b h j d -> b h i j", q, k) * (self.head_dim ** -0.5)
-        logit_residual = logits.mean(dim=1, keepdim=True)
-        logits = self.causal_bias + self.alibi(accumulated_logits + logits)
-        a = F.softmax(logits, dim=-1)
-        o = einsum("b h i j, b h j d -> b h i d", a, v)
-        o = rearrange(o, "b h i d -> b i (h d)")
-
-        input = self.mid_ln(p)
-        left = p[..., :self.hidden_dim]
-        right = p[..., self.hidden_dim:]
-        p = contract("...r, ...r, or -> ...o", left, right, self.mixing_tensor) * (self.hidden_dim ** -0.5)
-
+        (q, k, v) = map(lambda x: rearrange(x, "b i (h d) -> b i h d", h=self.heads), (q, k, v))
+        a = contract("b i h d, b j h d -> b h i j", q, k) * (self.head_dim ** -0.5)
+        a = self.causal_bias + self.alibi(a)
+        a = F.softmax(a, dim=-1)
+        o = contract("b h i j, b j h d -> b i h d", a, v)
+        o = rearrange(o, "b i h d -> b i (h d)")
+        p = contract("b i d, b i d -> b i d", F.gelu(p), torch.roll(p, 1, dims=-1))
         x = torch.cat([o, p], dim=-1)
         x = self.out_proj(x)
         x = self.out_ln(x)
-        return x, logit_residual
+        return x
 
 class Transformer(nn.Module):
     def __init__(self, config: Config):
         """
-        In this module is the code for the model as a whole, including the
-        prelude (where tokens become hidden representations), the body (where
-        hidden representations are transformed), and the postlude (where
-        hidden representations become token distributions).
+        In this module is the code for the model as a whole. Rather than
+        standard residual connections, in this design, each layer aggregates
+        the outputs of previous layers gated by a learned scalar. This scalar
+        is initialized to 1, which makes the default behavior the same as
+        normal residual connections, but allows the net to gate-off certain
+        contributions & construct its outputs from only certain layers.
         """
         super(Transformer, self).__init__()
-        self.config = config
         self.max_seq_len = config.max_seq_len
-        hidden_dim = config.heads * config.head_dim
         self.sos_index = config.vocab_size
+        hidden_dim = config.heads * config.head_dim
         
-        self.prelude = nn.Sequential(*[
-                                  nn.Embedding(config.vocab_size + 1, hidden_dim),
-                                  ])
-        self.body = nn.Sequential(*[Residual(Block(config, layer_depth)) for layer_depth in range(config.depth)])
-        self.postlude = nn.Sequential(*[
+        embedding = nn.Embedding(config.vocab_size + 1, hidden_dim)
+        unembedding = nn.Sequential(*[
                                   nn.LayerNorm((hidden_dim)),
                                   nn.Linear(hidden_dim, config.vocab_size, bias=True),
                                   ])
+        self.layers = nn.ModuleList([embedding] + [Block(config) for _ in range(config.depth)] + [unembedding])
+
+        self.gates = nn.ParameterList([nn.Parameter(torch.ones(i)) for i in range(len(self.layers))])
 
     def forward(self, x):
         b, i = x.shape
-        x = self.prelude(x)
-        attn = torch.zeros((b, 1, i, i), device=x.device)
-        for (i, layer) in enumerate(self.body):
-            x, attn = layer(x, attn)
-        x = self.postlude(x)
-        return x
+        outputs = []
+        for (i, layer) in enumerate(self.layers):
+            if i == 0: # Embedding Layer uses original input
+              x_in = x
+            else: # Non-embedding layers aggregate from previous layers; initialized as residual
+              x_in = contract("l, bidl -> bid", self.gates[i], torch.stack(outputs, -1))
+            outputs += [layer(x_in)]
+        return outputs[-1]
